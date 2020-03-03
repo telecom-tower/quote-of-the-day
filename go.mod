a09111c20c5680c7b522ba8c7a4eda595bab22cf
Jacques Supcik <jacques.supcik@hefr.ch>
2020-03-03 10:51:52 +0100

    fix

--- go.mod
+++ go.mod
@@ -1,19 +1,21 @@
 module "https://github.com/telecom-tower/quote-of-the-day"
 
-go 1.12
+go 1.14
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/golang/protobuf v1.3.4 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/namsral/flag v1.7.4-pre
-	github.com/pkg/errors v0.8.1
-	github.com/robfig/cron/v3 v3.0.0
+	github.com/pkg/errors v0.9.1
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/telecom-tower/sdk v1.0.0-alpha.1
 	github.com/telecom-tower/towerapi v1.0.0-alpha.2 // indirect
-	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	golang.org/x/sys v0.0.0-20200103143344-a1369afcdac7 // indirect
+	golang.org/x/image v0.0.0-20200119044424-58c23975cae1
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
 	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/genproto v0.0.0-20191230161307-f3c370f40bfb // indirect
-	google.golang.org/grpc v1.26.0
+	google.golang.org/genproto v0.0.0-20200302123026-7795fca6ccb1 // indirect
+	google.golang.org/grpc v1.27.1
 )
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/namsral/flag"
@@ -22,6 +23,8 @@ import (
 
 const (
 	quotesServer = "http://quotes.rest/qod.json"
+	secret       = "OneFlagToRuleThemAll"
+	mqttTopic    = "heiafriscctf"
 )
 
 type success struct {
@@ -82,32 +85,61 @@ func getQod(category string) (*qod, error) {
 		return nil, errors.WithMessage(err, "Invalid quote")
 	}
 
-	return &q, nil
-
-}
-
-func updateDisplay(grpcUrl string, q *qod) {
-	log.Infof("Connecting to tower server : %v", grpcUrl)
-	conn, err := grpc.Dial(grpcUrl, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Error dialing server: %v", err)
-	}
-	defer conn.Close() // nolint: errcheck
-	client := sdk.NewClient(conn)
-
-	check(client.StartDrawing(context.Background()), "Error getting frame")
-	check(client.Init(), "Error initializing display")
-
 	log.Infof("Quote: \"%v\"", q.Contents.Quotes[0].Quote)
 	log.Infof("Author: \"%v\"", q.Contents.Quotes[0].Author)
 
+	return &q, nil
+}
+
+func updateDisplay(client *sdk.Client, q *qod) {
+	log.Debug("Updating Display")
+	check(client.StartDrawing(context.Background()), "Error getting frame")
+	check(client.Init(), "Error initializing display")
 	format := "<text><font color=\"dogerblue\">%s</font> <font color=\"gold\">(%s)</font> <font color=\"lime\">&gt;&gt;&gt;</font> </text>"
 	msg := fmt.Sprintf(format, q.Contents.Quotes[0].Quote, q.Contents.Quotes[0].Author)
 	check(client.WriteText(msg, "6x8", 0, colornames.Dodgerblue, 0, sdk.PaintMode), "Error writing text")
 	check(client.AutoRoll(0, sdk.RollingNext, 0, 0), "Error setting autoroll")
 	check(client.Render(), "Error rendering")
-
 	log.Debug("Done")
+}
+
+func showSecret(client *sdk.Client, secret string) {
+	log.Debug("Show secret")
+	for _, c := range secret {
+		check(client.StartDrawing(context.Background()), "Error getting frame")
+		check(client.Init(), "Error initializing display")
+		check(client.WriteText(string(c), "8x8", 60, colornames.Orange, 0, sdk.PaintMode), "Error writing text")
+		check(client.AutoRoll(0, sdk.RollingStop, 0, 0), "Error setting autoroll")
+		check(client.Render(), "Error rendering")
+
+		time.Sleep(70 * time.Millisecond)
+
+		check(client.StartDrawing(context.Background()), "Error getting frame")
+		check(client.Init(), "Error initializing display")
+		check(client.Render(), "Error rendering")
+
+		time.Sleep(150 * time.Millisecond)
+	}
+	log.Debug("Done")
+}
+
+func serve(client *sdk.Client) (chan *qod, chan [2]string) {
+
+	qodQueue := make(chan *qod, 1)
+	secretQueue := make(chan [2]string, 1)
+
+	go func() {
+		for {
+			select {
+			case q := <-qodQueue:
+				updateDisplay(client, q)
+
+			case <-secretQueue:
+				showSecret(client, secret)
+			}
+		}
+	}()
+	return qodQueue, secretQueue
 }
 
 func main() {
@@ -125,15 +157,24 @@ func main() {
 	var q *qod
 	var err error
 
+	log.Infof("Connecting to tower server : %v", *grpcUrl)
+	conn, err := grpc.Dial(*grpcUrl, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Error dialing server: %v", err)
+	}
+	defer conn.Close() // nolint: errcheck
+
+	qodQueue, secretQueue := serve(sdk.NewClient(conn))
+
 	q, err = getQod(*category)
 	check(err, "Error getting Quote")
-	updateDisplay(*grpcUrl, q)
+	qodQueue <- q
 
 	c := cron.New()
-	c.AddFunc("5 * * * *", func() {
-		updateDisplay(*grpcUrl, q)
+	c.AddFunc("*/5 * * * *", func() {
+		qodQueue <- q
 	})
-	c.AddFunc("15 * * * *", func() {
+	c.AddFunc("2,32 * * * *", func() {
 		q, err = getQod(*category)
 		if err != nil {
 			log.Warn("Error updating quote : %v", err)
@@ -142,10 +183,22 @@ func main() {
 	c.Start()
 
 	opts := MQTT.NewClientOptions()
-	opts.AddBroker("broker.hivemq.com")
+	opts.AddBroker("mqtt.eclipse.org:1883")
 	opts.SetCleanSession(true)
 
-	mqtt := MQTT.NewClient(opts)
+	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
+		secretQueue <- [2]string{msg.Topic(), string(msg.Payload())}
+		qodQueue <- q
+	})
+
+	mqttClient := MQTT.NewClient(opts)
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := mqttClient.Subscribe(mqttTopic, byte(0), nil); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
 
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
